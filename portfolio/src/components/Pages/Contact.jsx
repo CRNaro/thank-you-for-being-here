8a26c5419528eb22afb94658d5e47f7a8c0cb801
CRNaro <144285637+CRNaro@users.noreply.github.com>
2024-03-04 10:29:31 -0500

    build: added a field required notice to my Contact page.

--- portfolio/src/components/Pages/Contact.jsx
+++ portfolio/src/components/Pages/Contact.jsx
@@ -1,22 +1,59 @@
 // This page will house all of my contact information.
+import { useState } from 'react';
  import '../../styles/Contact.css';
+ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Contact() {
+  const [nameTouched, setNameTouched] = useState(false);
+  const [emailTouched, setEmailTouched] = useState(false);
+  const [messageTouched, setMessageTouched] = useState(false);
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name === "" || email === "" || message === "") {
+      alert("Please fill out all fields");
+    } else {
+    console.log(name, email, message);
+    
+  }
+  }
+
   return (
     <div>
       <h1>Contact</h1>
       <p>Email:</p>
-      <form action= "https://formspree.io/CRNaro@gmail.com" method="POST">
-        <label class="name-field" >
-            <input type="text" name="name" placeholder="Your Name" />
+      <form onSubmit={handleSubmit} action= "https://formspree.io/CRNaro@gmail.com" method="POST">
+        <label className="name-field" >
+            <input type="text" 
+                    name="name" 
+                    className={`form-control ${nameTouched && name === "" ? "is-invalid" : ""}`} 
+                    placeholder={nameTouched && name === "" ? "Field required" : "Your Name"} 
+                    onChange={e => setName(e.target.value)}
+                    onBlur={() => setNameTouched(true)}
+                    />
         </label>
-        <label class="email-field" >
-            <input  type="email" name="_replyto" placeholder="Your Email" />
+        <label className="email-field" >
+            <input  type="email" 
+                    name="_replyto" 
+                    className={`form-control ${emailTouched && email === "" ? "is-invalid" : ""}`} 
+                    placeholder={emailTouched && email === "" ? "Field required" : "Your Email"} 
+                    onChange={e => setEmail(e.target.value)}
+                    onBlur={() => setEmailTouched(true)}
+                    />
         </label>
-        <label class="message-field" >
-            <textarea name="message" placeholder="Message Me"></textarea>
+        <label className="message-field" >
+            <textarea name="message" 
+           className={`form-control ${messageTouched && message === "" ? "is-invalid" : ""}`} 
+           placeholder={messageTouched && message === "" ? "Field required" : "Your Message"} 
+           onChange={e => setMessage(e.target.value)}
+            onBlur={() => setMessageTouched(true)}
+            ></textarea>
         </label>
-        <button type="submit">Send</button>
+        <button type="submit">Submit</button>
         </form>
     </div>
   );
--- portfolio/src/components/Pages/Home.jsx
+++ portfolio/src/components/Pages/Home.jsx
@@ -4,12 +4,42 @@ import selfImg from '../../assets/CRN.jpeg';
 
 function Home() {
     return (
-        <div>
+        <div class="welcome-content">
               <img class="header-img" src={selfImg} alt="Christopher Naro"  />
-            <h1>Home</h1>
+              <div >
+            <h1>Welcome to Christopher R Naro's Professional Webpage</h1>
             <p>
-                Welcome to my portfolio site. Here you can learn more about me and see some of the projects I have worked on.
+            
+Dear Visitor,
+<br/>
+Welcome, and thank you for taking the time to visit my professional webpage. 
+I am genuinely grateful for your interest in getting to know me and exploring 
+my work. 
+<br/>
+In a world filled with endless options, the fact that you've chosen to spend 
+your valuable time here means a great deal to me. Whether you're a colleague, 
+a potential collaborator, or simply curious about what I have to offer, I am 
+honored by your presence.
+<br/>
+This webpage is not just a collection of information about my career and
+accomplishments; it's a reflection of my passion, dedication, and commitment 
+to growing and learning in all facets of my life. This is a platform where I strive 
+to showcase my skills, experiences, and the projects that I'm truly proud of.
+As you navigate through these pages, I hope you'll gain insight into who I am 
+as a professional and as an individual. Whether you're seeking collaboration 
+opportunities, exploring potential partnerships, or simply looking to connect, 
+I'm excited about the possibility of us crossing paths and creating meaningful 
+connections.
+<br/>
+Once again, thank you for stopping by. Your visit is appreciated more than words 
+can express. Please feel free to reach out if you have any questions or if 
+there's anything I can assist you with.
+<br/>
+Warm regards,
+<br/>
+Christohper R Naro
             </p>
+            </div>
         </div>
     );
 }
