4f9783f3a8f718c995ba89ddb224727a03981e80
CRNaro <144285637+CRNaro@users.noreply.github.com>
2024-03-05 14:46:47 -0500

    Build: added my proficiencies to my resume page

--- portfolio/src/components/Pages/Resume.jsx
+++ portfolio/src/components/Pages/Resume.jsx
@@ -2,16 +2,31 @@
 
 function Resume() {
     return (
-        <div>
+        <section>
             <h1>Resume:</h1>
             <a href="https://docs.google.com/document/d/1QKkubolsRJ5f-EszamvKNPAMFrNY9PFUMHPy5h9DFrU/edit?usp=sharing">View & Download</a>
             <p>
                 If you have any further questions, please feel free to contact me.
             </p>
+            <div className='proficiencies'>
             <p>
-                My proficiencies include:
+               <h3>My proficiencies include:</h3> 
+               <ul>
+                     <li>HTML</li>
+                     <li>CSS</li>
+                     <li>JavaScript</li>
+                     <li>React</li>
+                     <li>Node</li>
+                     <li>Express</li>
+                     <li>MySQL</li>
+                     <li>MongoDB</li>
+                     <li>Bootstrap</li>
+                     <li>Handlebars</li>
+                     <li>jQuery</li>
+               </ul>
             </p>
-        </div>
+            </div>
+        </section>
     );
 }
 
