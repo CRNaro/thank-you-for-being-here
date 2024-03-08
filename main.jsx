f0cd725bc1f9e61307e0654fc3e97030321fe2a1
CRNaro <144285637+CRNaro@users.noreply.github.com>
2024-03-07 21:50:35 -0500

    Fixing CSS style sheets import.

--- main.jsx
+++ main.jsx
@@ -1,10 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './portfolio/src/App'
-import './portfolio/src/App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
